6f70947b094f62462ccfc28ed9d52361fac726b4
Gutcal <70690295+Gutcal@users.noreply.github.com>
2021-01-21 12:43:22 +0300

    Add files via upload

---
+++ pr 1.py
@@ -0,0 +1,9 @@
+x=[1,2,6,2,4,5,2,0,9,8,7]
+print('lista:',x)
+x.sort()
+print('crescator', x)
+x.sort(reverse=True)
+print('descrescator:',x)
+print('numar maxim:', max(x))
+print('numar minim:', min(x))
+print('lungimea listei',len(x))
